ed5aac406b3bf5ec88c41a4e6576cdc3987fac4e
User <user@example.com>
2026-03-27 15:43:14 +0800

    feat: 更新博客内容并重新构建部署
    - 更新 digital-products-roundup.mdx 博客内容
    - 更新 taobao-alcohol-rankings-2026.mdx 博客内容
    - 新增 whiskey-vodka-recommendation-2026.mdx 博客
    - 更新 affiliateLinks.ts 联盟链接配置
    - 更新 tag-data.json 标签数据
    - 重新构建生成 out 目录用于部署

--- data/affiliateLinks.ts
+++ data/affiliateLinks.ts
@@ -46,6 +46,21 @@ function generateTbLink(baseUrl: string): string {
 
 export const affiliateLinks: Record<string, AffiliateLink> = {
   // 数码产品
+  'fuji-gfx100-ii': {
+    jd: '', // 可后续补充京东链接
+    tb: 'https://s.click.taobao.com/t?e=m%3D2%26s%3DeaC%2Ba5sYb9Vw4vFB6t2Z2ueEDrYVVa64YUrQeSeIhnK53hKxp7mNFl906SyIHsHUcx6NMuQ%2BBwROS4pX4DGe7vc91BEt1OIeAp0l%2FtogXIvvSDU5KD8g6ckMvl%2BcriAg%2FgFD2Cb2HtzSmu0J2OYLCQSY28wYIhWGDTzfUZS4jc1rrakkZKnPqAtkyqhBb5%2B4HoaaUnJGJVPiT5R7M%2BfUrE0xJELmpcVjphfEY%2B7W6Vi%2F2Gggwtk4PMj9YP7JZC3H17mMd%2F9EYPNyOmv97bY0D4%2BDKwFLEd9Q5dUsQ8NYvbj%2B58h5pKGYu5UYXHAgtCa69KvR6od1T1EMlu5kMKse3g%3D%3D&union_lens=lensId%3APUB%401774575739%40212b05fb_0e45_19d2cf4e382_3552%40021hWt97BrWPHM2dpwQod4aG%40eyJmbG9vcklkIjo4MDkyNywiic3BtQiiI6Il9wb3J0YWxfdjJfcGFnZXNfcHJvbW9fc2hvcF9kZXRhaWxfaW5kZXhfaHRtIiiwiic3JjRmxvb3JJZCI6IjgwOTI3In0ie%3Bscm%3A1007.30148.329090.pub_search-item_883b1277-f6d8-4b41-b7f7-741b54a28cc5_;https%3A%2F%2Fimg.alicdn.com%2Fbao%2Fuploaded%2FO1CN012me7KO1gv1fGPrz6p_!!6000000004203-0-yinhe.jpg',
+    mobilePreference: 'tb'
+  },
+  'huawei-mate-80-pro-max': {
+    jd: '', // 可后续补充京东链接
+    tb: 'https://s.click.taobao.com/t?e=m%3D2%26s%3D2jO2fgfoqr1w4vFB6t2Z2ueEDrYVVa64YUrQeSeIhnK53hKxp7mNFl906SyIHsHUiMmRO9wDtCdOS4pX4DGe7vc91BEt1OIeAp0l%2FtogXIvvSDU5KD8g6ckMvl%2BcriAg%2FgFD2Cb2HtzSmu0J2OYLCQSY28wYIhWGDTzfUZS4jc1rrakkZKnPqAtkyqhBb5%2B4HoaaUnJGJVNeHAcdRdh%2BYtY2nClrNUdaopiJ8pas%2Bvnzy0fGSbe3sNwisUomr7%2Fuo8UQ3jdRiVRkrrFd%2BItQX0%2FuprW1TdmBLeMqtJBmsqDZGL0GbiMMdy4zRWxjykkuakcnG67xjzl4FEJj62OWMg%3D%3D&union_lens=lensId%3APUB%401774575739%40212b05fb_0e45_19d2cf4e382_3551%40022I71SDx2QS8bzDRPuBWCMi%40eyJmbG9vcklkIjo4MDkyNywiic3BtQiiI6Il9wb3J0YWxfdjJfcGFnZXNfcHJvbW9fc2hvcF9kZXRhaWxfaW5kZXhfaHRtIiiwiic3JjRmxvb3JJZCI6IjgwOTI3In0ie%3Bscm%3A1007.30148.329090.pub_search-item_883b1277-f6d8-4b41-b7f7-741b54a28cc5_;https%3A%2F%2Fimg.alicdn.com%2Fi1%2F2213856588863%2FO1CN01JoUsQd2FLJScS6boX_!!2213856588863.jpg',
+    mobilePreference: 'tb'
+  },
+  'iphone17-pro-max': {
+    jd: '', // 可后续补充京东链接
+    tb: 'https://s.click.taobao.com/t?e=m%3D2%26s%3DRdXZpmRA4ehw4vFB6t2Z2ueEDrYVVa64YUrQeSeIhnK53hKxp7mNFl906SyIHsHUeP1OzWF%2F2zxOS4pX4DGe7vc91BEt1OIeAp0l%2FtogXIvvSDU5KD8g6ckMvl%2BcriAg%2FgFD2Cb2HtzSmu0J2OYLCQSY28wYIhWGlWC%2BLUEi2rPuRqZVqZbGdAtkyqhBb5%2B4HoaaUnJGJVP%2FIkuGI6g2J%2FoPEhl8C8BkRFkUbuUNjUsEdmCX0p%2F1%2B1zPQhBfMNxTUMVab1vXPgc1eMMBy3yJBI%2BDKwFLEd9Q5dUsQ8NYvbj%2B58h5pKGYu6b9WxLveENMatJN3PTYhhzPZMUUR31Kpg%3D%3D&union_lens=lensId%3APUB%401774575123%40214697a2_1ab8_19d2ceb7bf2_b6df%40025hpE6R9NduDLet7blndIPC%40eyJmbG9vcklkIjo4MDY3NCwiic3BtQiiI6Il9wb3J0YWxfdjJfcGFnZXNfcHJvbW9fZ29vZHNfaW5kZXhfaHRtIiiwiic3JjRmxvb3JJZCI6IjgwNjc0In0ie%3BtkScm%3Asearch_fuzzy_selectionPlaza_site_4358_0_0_0_2_177457512348452195179%3Bscm%3A1007.30148.329090.pub_search-item_d4f66660-72bb-4c99-88bf-4ee47ef4c529_%3Bhttps%3A%2F%2Fimg.alicdn.com%2Fbao%2Fuploaded%2FO1CN01W0rnhS1jTxCGu5gXy_!!6000000004550-0-yinhe.jpg',
+    mobilePreference: 'tb'
+  },
   'iphone15-pro-max': {
     jd: generateJdLink('https://u.jd.com/xxxxx'),
     tb: generateTbLink('https://s.click.taobao.com/xxxxx'),
@@ -61,12 +76,22 @@ export const affiliateLinks: Record<string, AffiliateLink> = {
     tb: generateTbLink('https://s.click.taobao.com/macbooktb'),
     mobilePreference: 'jd'
   },
+  'macbook-air-m4': {
+    jd: '', // 可后续补充京东链接
+    tb: 'https://s.click.taobao.com/t?e=m%3D2%26s%3DkJrvAGGdXf9w4vFB6t2Z2ueEDrYVVa64YUrQeSeIhnK53hKxp7mNFl906SyIHsHU9OK3r0VQYcJOS4pX4DGe7vc91BEt1OIeAp0l%2FtogXIvvSDU5KD8g6ckMvl%2BcriAg%2FgFD2Cb2HtzSmu0J2OYLCQSY28wYIhWGlWC%2BLUEi2rPuRqZVqZbGdAtkyqhBb5%2B4HoaaUnJGJVP0Sb66CtFmv6GKamXLQN5NlDPyhjXZwreHDhAS32i42Mco1rcV2gLx1taTK4vPyPaqwocLtOyFR4%2BDKwFLEd9Q5dUsQ8NYvbj%2B58h5pKGYu4Bv8HDxWgDI94FOXsSgND4vfeUUl6%2F7pA%3D%3D&union_lens=lensId%3APUB%401774575123%40214697a2_1ab8_19d2ceb7bf2_b6de%40024ZioqfstafbEXnCXj4eIy1%40eyJmbG9vcklkIjo4MDY3NCwiic3BtQiiI6Il9wb3J0YWxfdjJfcGFnZXNfcHJvbW9fZ29vZHNfaW5kZXhfaHRtIiiwiic3JjRmxvb3JJZCI6IjgwNjc0In0ie%3BtkScm%3Asearch_fuzzy_selectionPlaza_site_4358_0_0_0_1_177457512348452195179%3Bscm%3A1007.30148.329090.pub_search-item_d4f66660-72bb-4c99-88bf-4ee47ef4c529_%3Bhttps%3A%2F%2Fimg.alicdn.com%2Fbao%2Fuploaded%2FO1CN01GBb7hu1HeCqhUUlys_!!6000000000782-0-yinhe.jpg',
+    mobilePreference: 'tb'
+  },
   
   // 家居电器
   'dyson-v15': {
     jd: generateJdLink('https://u.jd.com/dysonv15'),
     tb: generateTbLink('https://s.click.taobao.com/dysonv15'),
     mobilePreference: 'jd'
+  },
+  'samsung-qa85qn950fjxxz': {
+    jd: '', // 可后续补充京东链接
+    tb: 'https://s.click.taobao.com/t?e=m%3D2%26s%3DEsUpOmODk5lw4vFB6t2Z2ueEDrYVVa64YUrQeSeIhnK53hKxp7mNFl906SyIHsHUpFMcnztlbWBOS4pX4DGe7vc91BEt1OIeAp0l%2FtogXIvvSDU5KD8g6ckMvl%2BcriAg%2FgFD2Cb2HtzSmu0J2OYLCQSY28wYIhWGDTzfUZS4jc1rrakkZKnPqAtkyqhBb5%2B4HoaaUnJGJVM4KvGC3w%2B5PBrpr5Uk60G7vSN0VSNmkYnvg%2Fy9G8c5f6SnvR00%2F%2F3RtNq66TgU2AONwgmp5IDic0%2FuprW1TdmBLeMqtJBmsqDZGL0GbiMMdzbUPVI0z%2BVK2%2Foug9KGNEy9Gf2zmUiveQ%3D%3D&union_lens=lensId%3APUB%401774575739%40212b05fb_0e45_19d2cf4e382_354e%40023ikAtApH8JR7RQ7e22E5L9%40eyJmbG9vcklkIjo4MDkyNywiic3BtQiiI6Il9wb3J0YWxfdjJfcGFnZXNfcHJvbW9fc2hvcF9kZXRhaWxfaW5kZXhfaHRtIiiwiic3JjRmxvb3JJZCI6IjgwOTI3In0ie%3Bscm%3A1007.30148.329090.pub_search-item_883b1277-f6d8-4b41-b7f7-741b54a28cc5_%3Bhttps%3A%2F%2Fimg.alicdn.com%2Fbao%2Fuploaded%2Fi1%2F2217052187942%2FO1CN01jIhrZs28XUd2iPElj_!!2217052187942-0-scmitem361000.jpg',
+    mobilePreference: 'tb'
   },
   'xiaomi-robot': {
     jd: generateJdLink('https://u.jd.com/xiaomirobot'),
